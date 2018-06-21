14e470a4b791ba04f7655b01f48811aa1f073408
Abby Lortz <abilenelortz03@gmail.com>
2018-06-21 20:14:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file.
+let studentsNames = ['Amy', 'Cate', 'Emma', 'Jennifer', 'Natalie'];
+function other(students) {
+  return students.toLowerCase()
+}
+
+function lowerCaseStudentNames(names) {
+  return names.map(other)
+}
+
+const students = ['Amy Adams', 'Cate Blanchett', 'Emma Stone', 'Jennifer Lawrence', 'Natalie Portman'];
+
+function nameToAttributes(students)
+  return 
